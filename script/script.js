183de78499c14ad729b3581f27eb54989f64267d
Khrir <karllisson.15henrique@hotmail.com>
2021-12-23 23:00:01 -0300

    Att loc

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-const url_categoria_economica = 'https://raw.githubusercontent.com/khrir/Ideias-PWEB/main/Dashboard/database/Despesas_CE.csv';
-const url_favorecido = 'https://raw.githubusercontent.com/khrir/Ideias-PWEB/main/Dashboard/database/despesas_PF.csv';
-const url_municipio = 'https://raw.githubusercontent.com/khrir/Ideias-PWEB/main/Dashboard/database/Repasses_M.csv';
+const url_categoria_economica = 'https://raw.githubusercontent.com/khrir/Dashboard/main/database/Despesas_CE.csv';
+const url_favorecido = 'https://raw.githubusercontent.com/khrir/Dashboard/main/database/despesas_PF.csv';
+const url_municipio = 'https://raw.githubusercontent.com/khrir/Dashboard/main/database/Repasses_M.csv';
 
 async function test(url){
     fetch(url).then(function(response){return response.text();
